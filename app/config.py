2b4bdae9ebda634254a7c50413d7738c82c8d07c
mubaraizzat <mubaraizzat@gmail.com>
2026-07-26 15:03:59 +0400

    feat: initialize project structure and configure asynchronous database connectivity

---
+++ app/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDic
+
+
+_base_model=SettingsConfigDic(
+    env_file=".env",
+    env_ignore_empty=True,
+    extra="ignore"
+)
+class databseconfig(BaseSettings):
+    postgres_server:str
+    postgres_port:int
+    postgres_user:str
+    postgres_password:str
+    postgres_db:str
+
+    model_config=_base_model
+    @property
+    def async_db_url(self) -> str:
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_server}:{self.postgres_port}/{self.postgres_db}"
+
+    settingDB=databseconfig()
+    
